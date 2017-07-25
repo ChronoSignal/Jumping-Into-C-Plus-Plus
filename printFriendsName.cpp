499dd658a104b0dca1aec22276b0d74f0f755eff
Ryan Sullivan <chronosignal@gmail.com>
2017-07-25 14:41:44 -0400

    Add files via upload

---
+++ printFriendsName.cpp
@@ -0,0 +1,16 @@
+/*
+This is written by Ryan Sullivan
+
+This program is problem 2 from chapter 2 from the book Jumping into C++
+
+"Write a program that display multiple lines of text onto the screen, each one displaying the name of one of your friends"
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Jack"<<endl;
+    cout<<"Henry\n";
+    cout<<"Bob"<<endl;
+}
