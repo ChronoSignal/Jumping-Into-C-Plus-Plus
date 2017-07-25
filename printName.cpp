1b8d52aa0c9cab117446e48b42a49c51b9b29627
Ryan Sullivan <chronosignal@gmail.com>
2017-07-25 14:33:30 -0400

    Add files via upload

---
+++ printName.cpp
@@ -0,0 +1,15 @@
+/*
+Program written by Ryan Sullivan
+
+This program is a problem from the book Jumping into C++ by Alex Allain
+
+This is problem one from chapter two of the book.
+
+"Write a Program that prints out your name."
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Ryan Sullivan"<<endl;
+}
